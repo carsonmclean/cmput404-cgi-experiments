17e1f8be837a3f3eb923b306b5e84bf7e9e5dbf1
Carson McLean <camclean@ualberta.ca>
2016-09-22 14:19:48 -0600

    Adds cgi library and displays environment variables

--- cgi_script.py
+++ cgi_script.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
+import cgi
+
 # stdout is sent over HTTP to the browser
 print "Content-type: text/html"
 print
-print "<HTML><BODY><H1>Hello world!</H1></BODY></HTML>"
+print "<HTML><BODY><H1>Hello, world!</H1>"
+
+# The webserver talks back to the CGI program with environment variables
+print cgi.print_environ()
+
+print "</BODY></HTML>"
